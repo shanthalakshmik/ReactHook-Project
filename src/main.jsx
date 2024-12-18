d5116badd7538c8ba759dcedd3365b1f57826f31
shanthalaksmik <santhalakshmi0423@gmail.com>
2024-12-18 10:33:32 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Correct import for React 18
+import ReactDOM from 'react-dom/client'; 
 import './index.css'; // Import TailwindCSS
 import App from './App'; // Main App component
 
